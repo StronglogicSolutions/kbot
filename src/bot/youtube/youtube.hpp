43087413a663f7a50bdc1195be1ca7edbc0910a4
logicp <adventurist@gmail.com>
2020-10-24 11:23:44 -0400

    adding class for tracking conversation map

--- src/bot/youtube/youtube.hpp
+++ src/bot/youtube/youtube.hpp
@@ -23,30 +23,35 @@ const std::string DEFAULT_API_NAME{"YouTube Data API"};
  * Specializes in using the Google YouTube Data API
  *
  */
-class YouTubeBot : public Bot, public Worker {
- public:
- /**
+class YouTubeBot : public Bot, public Worker
+{
+public:
+  /**
   * @constructor
   */
   YouTubeBot()
-  : Bot("YouTubeBot"),
-    m_has_promoted(false),
-    m_is_own_livestream(false),
-    m_time_value(clock())
-  {}
+      : Bot("YouTubeBot"),
+        m_has_promoted(false),
+        m_is_own_livestream(false),
+        m_time_value(clock()),
+        m_conversations(Conversation::S{})
+  {
+  }
 
-  bool init() {
-    m_api               = GetAPI(DEFAULT_API_NAME);
-    YouTubeDataAPI* api = static_cast<YouTubeDataAPI*>(m_api.get());
+  bool init()
+  {
+    m_api = GetAPI(DEFAULT_API_NAME);
+    YouTubeDataAPI *api = static_cast<YouTubeDataAPI *>(m_api.get());
 
     if (
-      !api->FetchToken()      .empty() &&
-      !api->FetchLiveVideoID().empty() &&
-       api->FetchLiveDetails()
-    ) {
+        !api->FetchToken().empty() &&
+        !api->FetchLiveVideoID().empty() &&
+        api->FetchLiveDetails())
+    {
       api->FetchChatMessages();
 
-      if (api->GreetOnEntry()) {
+      if (api->GreetOnEntry())
+      {
         api->PostMessage("Hello");
       }
 
@@ -55,18 +60,22 @@ class YouTubeBot : public Bot, public Worker {
     return false;
   }
 
-  std::vector<std::string> CreateReplyMessages(LiveMessages messages, bool bot_was_mentioned = false) {
+  std::vector<std::string> CreateReplyMessages(LiveMessages messages, bool bot_was_mentioned = false)
+  {
     std::vector<std::string> reply_messages{};
-    YouTubeDataAPI* api = static_cast<YouTubeDataAPI*>(m_api.get());
+    YouTubeDataAPI *api = static_cast<YouTubeDataAPI *>(m_api.get());
     // if (bot_was_mentioned) {
-      auto reply_number = (!m_has_promoted) ? messages.size() + 1 : messages.size();
-      reply_messages.reserve(reply_number); // Create responses to all who mentioned bot specifically
+    auto reply_number = (!m_has_promoted) ? messages.size() + 1 : messages.size();
+    reply_messages.reserve(reply_number); // Create responses to all who mentioned bot specifically
 
-      for (const auto& message : messages) {
-        auto user_id = message.author;
-        if (!message.tokens.empty()) {
-          for (const auto& token : message.tokens) {
-            /**
+    for (const auto &message : messages)
+    {
+      auto user_id = message.author;
+      if (!message.tokens.empty())
+      {
+        for (const auto &token : message.tokens)
+        {
+          /**
             ┌───────────────────────────────────────────────────────────┐
             │░░░░░░░░░░░░░░░░░░░░░░TRACKING REPLIES░░░░░░░░░░░░░░░░░░░░░░│
             │░░░░░░░░Track if we have already responded to the author:░░░░░│
@@ -76,35 +85,43 @@ class YouTubeBot : public Bot, public Worker {
             └───────────────────────────────────────────────────────────┘
             */
 
-            if (token.type == TokenType::location //&&
-            //!api->HasInteracted(user_id, Interaction::location_ask)
-            ) {
-              reply_messages.push_back(CreateLocationResponse(token.value));
-              api->RecordInteraction(message.author, Interaction::location_ask, token.value);
-            }
-            else
-            if (token.type == TokenType::person //&&
-            //!api->HasInteracted(user_id, Interaction::greeting)
-            ) {
-              reply_messages.push_back(CreatePersonResponse(token.value));
-              api->RecordInteraction(message.author, Interaction::greeting, token.value);
-            }
-            else
-            if (token.type == TokenType::organization //&&
-            //!api->HasInteracted(user_id, Interaction::probing)
-            ) {
-              reply_messages.push_back(CreateOrganizationResponse(token.value));
-              api->RecordInteraction(message.author, Interaction::probing, token.value);
-            }
+          if (token.type == TokenType::location //&&
+              //!api->HasInteracted(user_id, Interaction::location_ask)
+          )
+          {
+            reply_messages.push_back(CreateLocationResponse(token.value));
+            api->RecordInteraction(message.author, Interaction::location_ask, token.value);
           }
+          else if (token.type == TokenType::person //&&
+                   //!api->HasInteracted(user_id, Interaction::greeting)
+          )
+          {
+            reply_messages.push_back(CreatePersonResponse(token.value));
+            api->RecordInteraction(message.author, Interaction::greeting, token.value);
+          }
+          else if (token.type == TokenType::organization //&&
+                   //!api->HasInteracted(user_id, Interaction::probing)
+          )
+          {
+            reply_messages.push_back(CreateOrganizationResponse(token.value));
+            api->RecordInteraction(message.author, Interaction::probing, token.value);
+          }
+
+          m_conversations.Insert(
+              Conversation::Message{
+                  .text = reply_messages.back(), .received = false},
+              message.author,
+              token.value);
         }
       }
+    }
     // } else {
     //   reply_messages.reserve((!m_has_promoted) ? 2 : 1);
     //   reply_messages.push_back("Hello from the KBot!");
     // }
 
-    if (!m_has_promoted) {
+    if (!m_has_promoted)
+    {
       reply_messages.push_back(CreatePromoteResponse());
       m_has_promoted = true;
     }
@@ -117,19 +134,23 @@ class YouTubeBot : public Bot, public Worker {
    *
    * The loop method runs on its own thread
    */
-  virtual void loop() override {
-    YouTubeDataAPI* api = static_cast<YouTubeDataAPI*>(m_api.get());
+  virtual void loop() override
+  {
+    YouTubeDataAPI *api = static_cast<YouTubeDataAPI *>(m_api.get());
 
     uint8_t no_hits{0};
 
-    while (m_is_running) {
+    while (m_is_running)
+    {
       api->ParseTokens();
 
-      if (api->HasChats()) {
+      if (api->HasChats())
+      {
         bool bot_was_mentioned = false;
         LiveMessages messages = api->FindMentions();
 
-        if (!messages.empty()) {
+        if (!messages.empty())
+        {
           bot_was_mentioned = true;
           log("Bot was mentioned");
         }
@@ -138,29 +159,38 @@ class YouTubeBot : public Bot, public Worker {
         api->ClearChat();
         // }
         auto k_api = GetAPI("Korean API");
-        KoreanAPI* korean_api = static_cast<KoreanAPI*>(k_api.get());
-        for (const auto& message : messages) {
-          if (korean_api->MentionsKorean(message.text)) {
+        KoreanAPI *korean_api = static_cast<KoreanAPI *>(k_api.get());
+        for (const auto &message : messages)
+        {
+          if (korean_api->MentionsKorean(message.text))
+          {
             log("Message from " + message.author + " mentions Korean:\n" + message.text);
           }
         }
 
         std::vector<std::string> reply_messages = CreateReplyMessages(messages, bot_was_mentioned);
         int max = 5;
-        for (const auto& reply : reply_messages) {
+        for (const auto &reply : reply_messages)
+        {
           m_posted_messages.push_back(reply);
           api->PostMessage(reply);
-          if (--max == 0) break;
+          if (--max == 0)
+            break;
         }
-      } else {
+      }
+      else
+      {
         no_hits++;
       }
 
       api->FetchChatMessages();
 
-      if (no_hits < 1000) {
+      if (no_hits < 1000)
+      {
         std::this_thread::sleep_for(std::chrono::milliseconds(30000));
-      } else {
+      }
+      else
+      {
         // Not having much luck. Take a break.
         no_hits = 0;
         std::this_thread::sleep_for(std::chrono::seconds(360));
@@ -174,70 +204,81 @@ class YouTubeBot : public Bot, public Worker {
    * @param
    * @returns
    */
-  virtual std::unique_ptr<API> GetAPI(std::string name = "") {
-    if (name.empty()) {
+  virtual std::unique_ptr<API> GetAPI(std::string name = "")
+  {
+    if (name.empty())
+    {
       return std::make_unique<DefaultAPI>();
     }
-    else
-    if (name.compare("Request API") == 0) {
+    else if (name.compare("Request API") == 0)
+    {
       return std::make_unique<RequestAPI>();
     }
-    else
-    if (name.compare("YouTube Data API") == 0) {
+    else if (name.compare("YouTube Data API") == 0)
+    {
       return std::make_unique<YouTubeDataAPI>();
     }
-    else
-    if (name.compare("Korean API") == 0) {
+    else if (name.compare("Korean API") == 0)
+    {
       return std::make_unique<KoreanAPI>();
     }
     return nullptr;
   }
 
-/**
+  /**
  * GetChats
  *
  * @returns [out] {LiveChatMap}  A map of Live Chats indexed by chat id
  */
-LiveChatMap GetChats() {
-  if (m_api != nullptr) {
-    return static_cast<YouTubeDataAPI*>(m_api.get())->GetChats();
+  LiveChatMap GetChats()
+  {
+    if (m_api != nullptr)
+    {
+      return static_cast<YouTubeDataAPI *>(m_api.get())->GetChats();
+    }
+    return LiveChatMap{};
   }
-  return LiveChatMap{};
-}
 
-/**
+  /**
  * PostMessage
  *
  * @param   [in]  {std::string}
  * @returns [out] {bool}
  */
-bool PostMessage(std::string message) {
-  m_posted_messages.push_back(message);
-  return static_cast<YouTubeDataAPI*>(m_api.get())->PostMessage(message);
-}
-
-std::string GetResults() {
-
-  std::string result{};
-
-  auto it     = std::make_move_iterator(m_posted_messages.begin());
-  auto end_it = std::make_move_iterator(m_posted_messages.end());
-
-  for (; it != end_it; it++) {
-    result += std::move(*it);
+  bool PostMessage(std::string message)
+  {
+    m_posted_messages.push_back(message);
+    return static_cast<YouTubeDataAPI *>(m_api.get())->PostMessage(message);
   }
 
-  return result;
-}
+  std::string GetResults()
+  {
+
+    std::string result{};
+
+    for (const auto m : m_conversations.GetConversations())
+    {
+      auto interlocutor = m.first.user;
+      auto subject      = m.first.subject;
+      auto message      = m.second->text;
+      auto was_received = m.second->received;
+
+      result += "Interlocutor" + interlocutor +
+                "\nSubject " + subject +
+                "\nMessage: " + message + "\n";
+    }
+    return result;
+  }
 
 private:
-  std::unique_ptr<API>     m_api;
-  bool                     m_is_own_livestream;
-  bool                     m_has_promoted;
-  std::condition_variable  m_work_thread_condition;
-  std::mutex               m_mutex;
-  clock_t                  m_time_value;
+  std::unique_ptr<API> m_api;
+  bool m_is_own_livestream;
+  bool m_has_promoted;
+  std::condition_variable m_work_thread_condition;
+  std::mutex m_mutex;
+  clock_t m_time_value;
   std::vector<std::string> m_posted_messages;
+  Conversation::S m_conversations;
 };
 
 #endif // __YOUTUBE_HPP__
--- src/util/nlp.hpp
+++ src/util/nlp.hpp
@@ -4,8 +4,10 @@
 #include <string>
 #include <cstring>
 #include <sstream>
+#include <vector>
+#include <map>
 
-const std::string TOKENIZER_PATH{""};
+const std::string TOKENIZER_PATH{"third_party/MITIE/tools/ner_stream/ner_stream third_party/MITIE/MITIE-models/english/ner_model.dat > tokenized_message.txt > tokenized_message.txt"};
 
 const std::string get_executable_cwd() {
   char* path = realpath("/proc/self/exe", NULL);
@@ -17,13 +19,91 @@ std::string TokenizeText(std::string s) {
   std::string execution_line{};
   execution_line.reserve(50);
 
-  execution_line += "echo \"" + s + "\" | third_party/MITIE/tools/ner_stream/ner_stream third_party/MITIE/MITIE-models/english/ner_model.dat > tokenized_message.txt > tokenized_message.txt";
+  execution_line += "echo \"" + s + "\" | " + TOKENIZER_PATH;
 
   std::system(execution_line.c_str());
     return std::string{
-        static_cast<std::stringstream const&>(
-            std::stringstream() << std::ifstream("tokenized_message.txt").rdbuf())
-            .str()};
+      static_cast<std::stringstream const&>(
+        std::stringstream() << std::ifstream("tokenized_message.txt").rdbuf())
+        .str()};
 }
 
+namespace Conversation {
+
+struct Context {
+std::string user;
+std::string subject;
+Context(std::string user_name, std::string subject_name) : user(user_name), subject(subject_name) {}
+
+bool operator <(const Context &rhs) const {
+  return (
+    user < rhs.user && subject < rhs.subject
+  );
+}
+
+};
+
+struct Message {
+  std::string text;
+  bool        received;
+  Message*    next;
+};
+
+class MyPair : public std::pair<Context, Message*> {
+  typedef std::pair<Context, Message*> parent_type;
+  public:
+  using parent_type::parent_type;
+
+  MyPair() : parent_type{Context{"", "" }, nullptr} {}
+};
+
+
+using Map            = std::map<Context, Message*>;
+using MessageObjects = std::vector<Message>;
+
+class S {
+
+public:
+S() : m_m{{Context("Initial", "place"), nullptr}} {}
+void Insert(Message&& node, std::string name, std::string subject) {
+  const Map::const_iterator it = m_m.find(Context{name, subject});
+  // Insert new head
+  if ( it == m_m.end()) {
+    m_m.insert({Context{name, subject}, &node});
+  } else {
+    Message* previous_head = it->second;
+
+    node.next = previous_head;
+    m_m.at({name, subject}) = &node;
+  }
+  m_v.emplace_back(std::move(node)); // Object lives in vector
+}
+
+void print() {
+  for (const auto& c : m_m) {
+    Message* node = c.second;
+
+    std::cout << "Interlocutor: " << c.first.user << "\nSubject: " << c.first.subject << std::endl;
+    while ( node != nullptr) {
+        std::cout << node->text;
+        node = node->next;
+    }
+    std::cout << std::endl;
+  }
+}
+
+Map GetConversations() {
+  return m_m;
+}
+
+Message* GetConversation(std::string name, std::string subject) {
+  return m_m.at(Context{name, subject});
+}
+
+private:
+  Map            m_m; // pointer map
+  MessageObjects m_v; // vector of objects
+};
+
+} // namespace Conversation
 #endif // __NLP_HPP
