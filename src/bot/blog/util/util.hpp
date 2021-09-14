a0a8c1b031dec5581a92a5539151df185c3bb66d
logicp <logicp@logicp.ca>
2021-09-13 21:35:13 -0400

    sanitizing string

--- src/bot/blog/util/util.hpp
+++ src/bot/blog/util/util.hpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <cpr/cpr.h>
 #include <fstream>
+#include <algorithm>
 
 namespace kbot
 {
@@ -11,9 +12,16 @@ static void SaveToFile(const std::string& data, const std::string& path)
 }
 static const std::string FetchFile(const std::string& full_url, const std::string& path, const bool verify_ssl = true)
 {
-  auto ext_end = full_url.find_first_of('?');
-       ext_end = ext_end == std::string::npos ? full_url.size() : ext_end;
-  const auto url     = full_url.substr(0, ext_end);
+  const auto SanitizeString = [](std::string s) -> std::string
+  {
+    const auto target{"\\"};
+    s.erase(std::remove(s.begin(), s.end(), '\\'), s.end());
+    return s;
+  };
+  auto s_url   = SanitizeString(full_url);
+  auto ext_end = s_url.find_first_of('?');
+       ext_end = ext_end == std::string::npos ? s_url.size() : ext_end;
+  const auto url     = s_url.substr(0, ext_end);
   const auto ext_beg = url.find_last_of('.');
   const auto ext_len = (ext_beg != url.npos) ? (url.size() - ext_beg) : 0;
   const auto fln_beg = url.find_last_of('/');
@@ -21,7 +29,7 @@ static const std::string FetchFile(const std::string& full_url, const std::strin
   if (fln_beg != std::string::npos)
   {
     const std::string   filename = url.substr(fln_beg + 1);
-    const cpr::Response r        = cpr::Get(cpr::Url{full_url}, cpr::VerifySsl(verify_ssl));
+    const cpr::Response r        = cpr::Get(cpr::Url{s_url}, cpr::VerifySsl(verify_ssl));
     if (r.error.code == cpr::ErrorCode::OK)
     {
       SaveToFile(r.text, filename);
