3cc006093567e9f6804e175cb0aae943f90dde58
logicp <logicp@logicp.ca>
2022-04-01 20:27:46 -0400

    able to pass username/password/room_id through config

--- src/bot/matrix/matrix.hpp
+++ src/bot/matrix/matrix.hpp
@@ -1,8 +1,43 @@
 #pragma once
 
+#include <INIReader.h>
 #include "katrix.hpp"
 #include "interfaces/interfaces.hpp"
 
+namespace katrix
+{
+namespace constants {
+static const uint8_t     APP_NAME_LENGTH{6};
+static const char*       DEFAULT_CONFIG_PATH{"config/config.ini"};
+} // namespace constants
+
+static std::string get_executable_cwd()
+{
+  std::string full_path{realpath("/proc/self/exe", NULL)};
+  return full_path.substr(0, full_path.size() - (constants::APP_NAME_LENGTH  + 1));
+}
+
+static const INIReader GetConfig()
+{
+  return INIReader{get_executable_cwd() + "/../" + constants::DEFAULT_CONFIG_PATH};
+}
+
+static std::string GetUsername()
+{
+  return GetConfig().GetString("matrix_bot", "username", "");
+}
+
+static std::string GetPassword()
+{
+  return GetConfig().GetString("matrix_bot", "password", "");
+}
+
+static std::string GetRoomID()
+{
+  return GetConfig().GetString("matrix_bot", "room_id", "");
+}
+
+} // ns katrix
 namespace kbot {
 class MatrixBot : public kbot::Worker,
                   public kbot::Bot,
@@ -10,22 +45,32 @@ class MatrixBot : public kbot::Worker,
 {
 public:
   MatrixBot()
-  : kbot::Bot{"matrixuser"},
-    katrix::KatrixBot{"matrix.org", [this](const katrix::EventID&, katrix::RequestError e)
+  : kbot::Bot{"logicp"},
+    katrix::KatrixBot{
+      "matrix.org",
+      katrix::GetUsername(),
+      katrix::GetPassword(),
+      [this](const katrix::EventID&, katrix::RequestError e)
       {
-        m_send_event_fn(CreateSuccessEvent(BotRequest{}));
+        m_send_event_fn((e) ? CreateErrorEvent  (katrix::error_to_string(e), m_last_request) :
+                              CreateSuccessEvent(m_last_request));
       }
-    }
+    },
+    m_room_id(katrix::GetRoomID())
   {}
 
   virtual void Init() override
   {
-    return;
+    katrix::KatrixBot::login();
   }
 
   virtual void loop() override
   {
-
+    if (katrix::KatrixBot::logged_in())
+    {
+      while (IsRunning())
+        katrix::KatrixBot::run();
+    }
   }
 
   void SetCallback(BrokerCallback cb_fn)
@@ -33,23 +78,21 @@ public:
     m_send_event_fn = cb_fn;
   }
 
-  enum class TGCommand
-  {
-  message   = 0x00,
-  poll      = 0x01,
-  poll_stop = 0x02
-  };
-
   bool HandleEvent(BotRequest request)
   {
     using Message = katrix::MessageType;
-    static const std::string room_id = "12345";
-    if (request.event == "send message")
+
+    try
     {
-      katrix::KatrixBot::send_message(room_id, Message{request.data});
+      katrix::KatrixBot::send_message(m_room_id, Message{request.data});
+      m_last_request = request;
       return true;
     }
-    return false;
+    catch(const std::exception& e)
+    {
+      log("Exception thrown: ", e.what());
+      return false;
+    }
   }
 
   virtual std::unique_ptr<API> GetAPI(std::string name) override
@@ -64,6 +107,7 @@ public:
 
   virtual void Start() override
   {
+    while (!katrix::KatrixBot::logged_in());
     if (!m_is_running)
       Worker::start();
   }
@@ -75,5 +119,7 @@ public:
 
 private:
   BrokerCallback m_send_event_fn;
+  BotRequest     m_last_request;
+  std::string    m_room_id;
 };
 } // namespace kbot
