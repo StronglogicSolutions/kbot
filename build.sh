a98f33e074e36e3220fce8afddc144b75a0441af
logicp <logicp@logicp.ca>
2022-05-16 19:56:01 -0400

    cleanup and adding a couple more patchelf commands to build script

--- build.sh
+++ build.sh
@@ -10,7 +10,9 @@ patchelf --replace-needed libkscord_lib.so third_party/kscord/out/libkscord_lib.
 patchelf --replace-needed libkstodon_lib.so third_party/kstodon/out/libkstodon_lib.so out/broker
 patchelf --replace-needed libkscord_lib.so third_party/kscord/out/libkscord_lib.so out/broker
 patchelf --replace-needed libktube_lib.so third_party/ktube/out/libktube_lib.so out/broker
+patchelf --replace-needed libktube_lib.so third_party/ktube/out/libktube_lib.so out/youtube_bot
 patchelf --replace-needed libmatrix_client.so.0.7.0 third_party/katrix/build/third_party/mtxclient/libmatrix_client.so.0.7.0 out/broker
 patchelf --replace-needed libTgBot.so.1 third_party/keleqram/third_party/tgbot-cpp/libTgBot.so.1 out/broker
+patchelf --replace-needed libTgBot.so.1 third_party/keleqram/third_party/tgbot-cpp/libTgBot.so.1 out/telegram_bot
 patchelf --replace-needed libktube_lib.so third_party/ktube/out/libktube_lib.so out/ut_kbot
 
